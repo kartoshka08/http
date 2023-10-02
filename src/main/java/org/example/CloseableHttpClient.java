b27332bb8a5435f48524050244735553846a775c
Kartoshka <kartoshka0811@gmail.com>
2023-10-03 01:45:41 +0300

    Initial commit

---
+++ src/main/java/org/example/CloseableHttpClient.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class CloseableHttpClient {
+    public static void main(String[] args) throws IOException {
+        org.apache.http.impl.client.CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setUserAgent("My Test Service")
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)
+                        .setSocketTimeout(30000)
+                        .setRedirectsEnabled(false)
+                        .build())
+                .build();
+        HttpGet request = new HttpGet("https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats");
+        request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
+        CloseableHttpResponse response = httpClient.execute(request);
+        Arrays.stream(response.getAllHeaders()).forEach(System.out::println);
+        List<PostJava> posts = mapper.readValue(response.getEntity().getContent(), new TypeReference<List<PostJava>>() {
+        });
+        posts.forEach(System.out::println);
+    }
+
+    public static ObjectMapper mapper = new ObjectMapper();
+}
